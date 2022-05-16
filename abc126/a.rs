951aad35b39162bc611dc83cf1a22a5d0f949c2c
semigrp <semigroupoid@gmail.com>
2022-05-17 01:12:49 +0900

    adw

--- abc126/a.rs
+++
@@ -1,8 +0,0 @@
-use std::ascii::AsciiExt;
- 
-fn main() {
-    let (_, K) = read!(usize, usize);
-    let mut S = rl().chars().collect::<Vec<_>>();
-    S[K - 1].make_ascii_lowercase();
-    println!("{}", S.into_iter().collect::<String>())
-}
