891c3ecbfe747e6706fece705b17defa492c728d
semigrp <semigroupoid@gmail.com>
2022-05-17 08:16:19 +0900

    98

---
+++ cpp/abc/abc123/b/main.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+
+using namespace std;
+// using namespace atcoder;
+using ll = long long;
+const ll MOD = 1000000007LL;
+// const ll MOD = 998244353LL;
+
+const vector<pair<int, int>> dpos4 = {{-1, 0}, {0, -1}, {0, 1}, {1, 0}};
+// const vector<pair<int, int>> dpos8 = {{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	return 0;
+}
