db72b346c64fd7ec5dec90d130d5be95d780f898
semigrp <semigroupoid@gmail.com>
2022-05-20 16:52:58 +0900

    awad

--- abc197/src/bin/a.rs
+++ abc197/src/bin/a.rs
@@ -1,3 +1,12 @@
+use std::io;
+
 fn main() {
-    todo!();
+  let x = get_input();
+  println!("{}", x * 2);
+}
+
+fn get_input() -> i32 {
+    let mut number = String::new();
+    io::stdin().read_line(&mut number).ok();
+    return number.trim().parse().ok().unwrap();
 }
