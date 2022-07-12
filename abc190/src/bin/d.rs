9cd3a283470d225bec106ad63253676a68065045
semigrp <semigroupoid@gmail.com>
2022-07-12 20:49:50 +0900

    d

--- abc190/src/bin/d.rs
+++ abc190/src/bin/d.rs
@@ -1,3 +1,18 @@
+use std::usize;
+
+#[allow(unused_imports)]
+use proconio::{
+    fastout, input,
+    marker::{Bytes, Chars, Isize1, Usize1},
+};
+
+#[fastout]
 fn main() {
-    todo!();
+    input! {n:i128};
+
+    let ans = enum_divisors(n);
+
+    println!("{}", ans.iter().filter(|x| *x % 2 == 1).count() * 2);
 }
+
+pub fn enum_divisors(n: i128) -> Vec<usize> {}
