b6df85dc4abc82eaa41037decfa1c5275e90b762
semigrp <semigroupoid@gmail.com>
2022-07-12 21:05:30 +0900

    d-fix

--- abc190/src/bin/d.rs
+++ abc190/src/bin/d.rs
@@ -8,11 +8,24 @@ use proconio::{
 
 #[fastout]
 fn main() {
-    input! {n:i128};
+    input! {n:usize};
 
     let ans = enum_divisors(n);
 
     println!("{}", ans.iter().filter(|x| *x % 2 == 1).count() * 2);
 }
 
-pub fn enum_divisors(n: i128) -> Vec<usize> {}
+pub fn enum_divisors(n: usize) -> Vec<usize> {
+    let mut res = vec![];
+    let mut i = 1;
+    while i * i <= n {
+        if n % i == 0 {
+            res.push(i as usize);
+            if i * i != n {
+                res.push(n / i as usize);
+            }
+        }
+        i += 1;
+    }
+    res
+}
