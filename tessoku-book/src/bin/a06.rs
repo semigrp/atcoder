f18dedc2b5b9bf413f0a25003fd46d2b3088c9a6
semigrp <semigroupoid@gmail.com>
2025-06-10 07:52:09 +0900

    a06 & a07

--- tessoku-book/src/bin/a06.rs
+++ tessoku-book/src/bin/a06.rs
@@ -6,5 +6,20 @@ use proconio::{fastout, input, marker::{Chars, Usize1}};
 #[fastout]
 fn main() {
     input! {
+        N: usize,
+        Q: usize,
+        A: [i32; N],
+        LR: [(i32, i32); Q],
+    }
+
+    let mut sums = vec![0; N + 1];
+
+    for i in 0..N {
+        sums[i + 1] = sums[i] + A[i];
+    }
+
+    for &(l, r) in &LR {
+        let result = sums[r as usize] - sums[(l - 1) as usize];
+        println!("{}", result);
     }
 }
--- tessoku-book/src/bin/a07.rs
+++ tessoku-book/src/bin/a07.rs
@@ -6,5 +6,21 @@ use proconio::{fastout, input, marker::{Chars, Usize1}};
 #[fastout]
 fn main() {
     input! {
+        D: usize,
+        N: usize,
+        LR: [(usize, usize); N],
     }
+
+    let mut dsums = vec![0i32; D + 2];
+    
+    for &(l, r) in &LR { 
+        dsums[l] += 1;
+        dsums[r + 1] -= 1;
+    }
+
+    for i in 1..=D {
+        dsums[i] += dsums[i - 1];
+        println!("{}", dsums[i]);
+    }
+
 }
