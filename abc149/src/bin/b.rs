75a24893f46620d3f9a9b51348122c15649aecf4
semigrp <semigroupoid@gmail.com>
2025-06-18 08:30:47 +0900

    abcqro

---
+++ abc149/src/bin/b.rs
@@ -0,0 +1,16 @@
+#![allow(non_snake_case)]
+#![allow(unused_imports)]
+
+use proconio::{fastout, input, marker::{Chars, Usize1}};
+
+#[fastout]
+fn main() {
+    input! {
+        a: usize,
+        b: usize,
+        k: usize,
+    }
+
+    for 
+}
+
