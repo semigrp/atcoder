d4879aa5bb91b156eee36923983753e0734816b6
semigrp <semigroupoid@gmail.com>
2025-06-18 08:10:20 +0900

    bpq

--- tessoku-book/src/bin/b01.rs
+++ tessoku-book/src/bin/b01.rs
@@ -6,5 +6,11 @@ use proconio::{fastout, input, marker::{Chars, Usize1}};
 #[fastout]
 fn main() {
     input! {
+        A: i32,
+        B: i32,
     }
+
+    let result = A + B;
+
+    println!("{}", result);
 }
