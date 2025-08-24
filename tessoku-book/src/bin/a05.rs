e629febbdd78ba5c7cf0c5d010396e430c4beb09
semigrp <semigroupoid@gmail.com>
2025-08-25 08:10:55 +0900

    add a05

--- tessoku-book/src/bin/a05.rs
+++ tessoku-book/src/bin/a05.rs
@@ -1,10 +1,24 @@
 #![allow(non_snake_case)]
 #![allow(unused_imports)]
+use proconio::{fastout, input};
 
-use proconio::{fastout, input, marker::{Chars, Usize1}};
 
 #[fastout]
 fn main() {
     input! {
+        N: i64,
+        K: i64,
     }
+
+    let mut ans: i64 = 0;
+
+    for i in 1..=N{
+        for j in 1..=N {
+            let w = K - i - j;
+            if 1 <= w && w <= N {
+                ans += 1;
+            }
+        }
+    }
+    println!("{}", ans);
 }
